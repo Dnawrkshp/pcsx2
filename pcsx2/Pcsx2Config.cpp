f9861b71017f75f15f04b975ef3cb66ec071a768
Daniel Gerendasy <danielgerendasy@gmail.com>
2024-12-31 13:26:24 -0800

    Fix Settings path never being set

--- pcsx2/Pcsx2Config.cpp
+++ pcsx2/Pcsx2Config.cpp
@@ -2121,11 +2121,6 @@ std::string EmuFolders::GetPortableModePath()
 
 bool EmuFolders::SetDataDirectory(Error* error)
 {
-	if (!DataRoot.empty())
-	{
-		return FileSystem::EnsureDirectoryExists(DataRoot.c_str(), false, error);
-	}
-
 	if (!ShouldUsePortableMode())
 	{
 #if defined(_WIN32)
