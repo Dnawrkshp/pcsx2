280022301212ba6e2079bb27b02054334cbe39bb
Daniel Gerendasy <danielgerendasy@gmail.com>
2026-02-24 17:40:08 -0800

    add null check to SPU2::GetOutputVolume and SPU2::SetOutputVolume

--- pcsx2/SPU2/spu2.cpp
+++ pcsx2/SPU2/spu2.cpp
@@ -136,12 +136,16 @@ void SPU2::UpdateSampleRate()
 
 u32 SPU2::GetOutputVolume()
 {
+	if (!s_output_stream)
+		return 0;
+
 	return s_output_stream->GetOutputVolume();
 }
 
 void SPU2::SetOutputVolume(u32 volume)
 {
-	s_output_stream->SetOutputVolume(volume);
+	if (s_output_stream)
+		s_output_stream->SetOutputVolume(volume);
 }
 
 u32 SPU2::GetResetVolume()
